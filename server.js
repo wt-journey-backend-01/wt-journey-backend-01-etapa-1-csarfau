a96c27d662da61dfc808795a2006badb5840eba4
Cesar <csarfau@gmail.com>
2025-07-13 01:12:09 -0300

    feat: basic api with requested routes

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (_, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/sugestao', (req, res) => {
+    const { nome, ingredientes } = req.query;
+
+    res.status(200).send(`
+        <h1>Obrigado pela sugestão!</h1>
+        <h2>Dados da sugestão enviada: </h2>
+        <p><b>Nome do lanche:</b> ${nome}</p>
+        <p><b>Ingredientes:</b> ${ingredientes}</p>
+        <p>
+            <a href="/">Voltar para página inicial</a>
+        </p>
+    `);
+});
+
+app.get('/contato', (_, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'views', 'contato.html'));
+});
+
+app.post('/contato', (req, res) => {
+    const { nome, email, assunto, mensagem } = req.body;
+    res.status(200).send(`
+        <h1>Mensagem recebida com sucesso! </h1>
+        <h2>Iremos analisar e entrar em contato o mais breve possível.</h2>
+        <h2>Informações enviadas:</h2>
+        <p><b>Nome:</b> ${nome}</p>
+        <p><b>E-mail:</b> ${email}</p>
+        <p><b>Assunto:</b> ${assunto}</p>
+        <p><b>Mensagem:</b> ${mensagem}</p>
+        <p>
+            <a href="/">Voltar para página inicial</a>
+        </p>
+    `);
+});
+
+app.get('/api/lanches', (_, res) => {
+    const filePath = path.join(__dirname, 'data', 'cardapio.json');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Erro ao ler o arquivo:', err);
+            return res.status(500).send('Erro ao processar sua solicitação.');
+        }
+
+        try {
+            const cardapioData = JSON.parse(data);
+            res.status(200).json(cardapioData);
+        } catch (parseError) {
+            console.error('Erro ao analisar o JSON:', parseError);
+            res.status(500).send('Erro: O formato do arquivo JSON é inválido.');
+        }
+    });
+});
+
+app.use((_, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor da DevBurger rodando em localhost:${PORT}`);
+});
